8c989bffb8d544f1e3483fb6b004c450367b8794
TarunReddyKalluri <55652043+tarunreddy6@users.noreply.github.com>
2023-03-01 17:19:57 +0530

    Update Streamlit_app.py
    added additional lines

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header("Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry')
+streamlit.text("Corn with Milk")
+streamlit.text('Oats')           
+           
